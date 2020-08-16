85574c20efa720a2be14277c095d9197569d7bf1
Mehmet Fatih Yıldız <fatih@mfyz.com>
2020-08-16 23:14:19 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import Layout from '../components/layout'
+
+const About = () => (
+	<Layout>
+		<Head>
+        	<title>About page title</title>
+		</Head>
+		<h1>About</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia aliquid obcaecati maxime ratione amet iure dolor architecto. Sequi non repellendus, provident vitae nemo tenetur ullam natus quisquam fugiat minima quia.
+		</p>
+	</Layout>
+)
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+const Index = () => (
+	<Layout>
+		<h1>Hello!</h1>
+	</Layout>
+)
+
+export default Index
